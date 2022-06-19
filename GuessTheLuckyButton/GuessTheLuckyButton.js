78c1f91052cf4a758201b68bbdfd1afb0589dc45
cristi3u <106305283+cristi3u@users.noreply.github.com>
2022-06-19 20:58:21 +0300

    Update GuessTheLuckyButton.js

--- GuessTheLuckyButton/GuessTheLuckyButton.js
+++ GuessTheLuckyButton/GuessTheLuckyButton.js
@@ -1,24 +1,24 @@
 function createButtons() {
-	let buttonsNumber = document.getElementById("valueId").value;
-	let randomValue = Math.floor(Math.random() * buttonsNumber) + 1;
-	for (let i = 1; i <= buttonsNumber; ++i) {
-		let btn = document.createElement("button");
-		btn.innerHTML = "Click me!";
-		btn.type = "submit";
-		btn.name = "luckyButton";
-		let myDiv = document.getElementById("buttonFeaturesId");
-		myDiv.appendChild(btn);
-		btn.onclick = function() {
-			findTheLuckyButton(i, randomValue)
-		};
-	}
-	document.getElementById('buttonStop').disabled = true;
+    let buttonsNumber = document.getElementById("valueId").value;
+    let randomValue = Math.floor(Math.random() * buttonsNumber) + 1;
+    for (let i = 1; i <= buttonsNumber; ++i) {
+	let btn = document.createElement("button");
+	btn.innerHTML = "Click me!";
+	btn.type = "submit";
+	btn.name = "luckyButton";
+	let myDiv = document.getElementById("buttonFeaturesId");
+	myDiv.appendChild(btn);
+	btn.onclick = function() {
+	findTheLuckyButton(i, randomValue)
+	};
+    }
+    document.getElementById('buttonStop').disabled = true;
 }
 
 function findTheLuckyButton(i, randomValue) {
-	if (i == randomValue) {
-		alert("You won!");
-	} else {
-		alert("You lost!")
-	}
+    if (i == randomValue) {
+	alert("You won!");
+    } else {
+	alert("You lost!")
+    }
 }
